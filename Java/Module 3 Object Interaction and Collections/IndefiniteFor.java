e2bbb4719d7a9e57ab3ade1142b1d3f7852ddc1b
Tristan Sim <tsym0098@gmail.com>
2024-11-06 22:53:22 +0800

    IndefiniteFor

---
+++ Java/Module 3 Object Interaction and Collections/IndefiniteFor.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner; 
+
+public class IndefiniteFor {
+
+/* Task: Write the code for such a for loop that keeps repeating to print out a number until the user tells the loop to exit.  */
+
+    public IndefiniteFor() {
+    }
+
+    public static void main(String[] args) 
+    {
+        // Declare Local Variables
+        String userInput; 
+        Scanner scanner = new Scanner(System.in);
+
+        for (int iterI = 0; ; iterI++) {
+
+            System.out.println("Current Count: " + iterI);
+
+            System.out.println("Continue Execution? [Y/N]");
+            userInput = scanner.nextLine(); 
+
+            if (userInput.equalsIgnoreCase("N")) {
+                break; 
+            }
+        }
+        
+        System.out.println("Loop Terminated...");
+        scanner.close();
+    }
+
+}
