77a3d7105728204d4176d3fa4bcbe17a6f02e7da
Tristan Sim <tsym0098@gmail.com>
2024-11-17 17:54:12 +0800

    Updated Unviersity - Work in Progress

--- Java Programming/Assesment Rolling Project/src/University.java
+++ Java Programming/Assesment Rolling Project/src/University.java
@@ -5,55 +5,85 @@
  * @version 1.00
  */
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.stream.IntStream; 
 
 public class University
 {
     // Fields
-    private Enrollment enrollment; 
+    ArrayList<Enrollment> enrollments;
 
     /**
      * Default Constructor
      */
     public University() {
-        enrollment = new Enrollment(); 
+        enrollments = new ArrayList<Enrollment>(); 
     }
 
     /**
      * Non-Defualt Constructor
-     * @param enrollment The New Enrollment Information as an Object of the Enrollment Class
+     * @param enrollment The New Enrollment Information as an Array List of Objects of the Enrollment Class
      */
-    public University(Enrollment enrollment) {
-        this.enrollment = enrollment; 
+    public University(ArrayList<Enrollment> enrollments) {
+        this.enrollments = enrollments; 
     }
 
     /**
      * Display Method: Display the University Information
+     * @return A String Output of the Array List of Enrollment of Ojects
      */
-    public String displayUniversity() {
-        return this.enrollment.displayEnrollment(); 
+    public String displayUniversity() 
+    {
+        StringBuffer enrollmentText = new StringBuffer(); 
+        for (Enrollment enrollment:this.enrollments)
+        {
+            if (enrollment != null) {
+                enrollmentText.append(enrollments); 
+            }
+        }
+        return enrollmentText.toString(); 
+    }
+    
+    /** 
+     * Custom Method: Add a New Enrollment into the Array List
+     * @param enrollment The new Enrollment Information to be added as an Object of te Enrollment Class
+     */
+    public void addEnrollment(Enrollment enrollment)
+    {
+        this.enrollments.add(enrollment); 
     }
 
     /** 
      * Accessor Method: Get the Enrollment Details
-     * @return the Enrollment Information as a object of the Enrollment Class
+     * @return The Enrollment Information as a Array List of Objects of the Enrollment Class
      */
-    public Enrollment getEnrollment() {
-        return this.enrollment; 
+    public ArrayList<Enrollment> getEnrollment() 
+    {
+        return this.enrollments; 
     }
-    
-    /* Now write the inputStudentDetails() method.
-       The purpose of this method is to accept student information by asking the user to enter all the 
-       student information one at a time while performing the following validations on the inputs:
 
-       For the Student object:
-       name, address, phoneNo, and email cannot be blank.
-       If any of the validations fail, print a message on the screen and assign the default value specified in the default constructor.
-       The method then assigns these values to the student fields in the enrolment class.
-        
-       Hint: You can achieve this by obtaining the object first from the Enrolment class via a method 
-       and then using that object to call another method in the Student class to set the value. Which method would that be?
-   */
+    /** 
+     * Accessor Method: Get the Enrollment Size
+     * @return The size of the the Enrollment Arraylist as an Integer
+     */
+    public int getEnrollmentSize()
+    {
+        return this.enrollments.size(); 
+    }
+
+    /** 
+     * Accesor Method: Get the Specific Enrollment Details
+     * @param index The Index of th Enrollment Object in the Arrray List as an Integer
+     * @return The Enrollment as an Object information at the Index of the Array List
+     * @throw IndexOutOfBoundsException The Index Input out ouf Bounds of the Array List Size
+     */
+    public Enrollment getSpecificEnrollment(int index)
+    {
+        if (index < 0 || index >= this.enrollments.size()) {
+            throw new IndexOutOfBoundsException("The Index is out of bound of the Enrollment Array List");
+        }
+        return this.enrollments.get(index); 
+    }
 
    /** 
     * Custom Method: Method to get User Input for the Student Details
@@ -92,35 +122,40 @@ public class University
         student.setEmail(userResponse[3]);
     }
 
-    /* 
-     * Now write the inputUnitDetails() method.
-     * The purpose of this method is to accept unit information by asking the user to enter all the unit information one at a time 
-     * while performing the following validations on the inputs via the Input class:
+    /**
+     * Custom Method: Get User to Input to Enrol Student
+     * @param enrollments The Enrollments Array Lists to be updated for Inputs by the User
+     */
+    public void inputUnitDetails(ArrayList<Enrollment> enrollments)
+    {   
+        // Check if enrollment has values 
+        if (enrollments == null || enrollments.isEmpty()) {
+            System.out.print("The Enrollment ArrayList is Empty"); 
+        }
 
-     * For the Unit object:
-     * unitCode cannot be blank and must be exactly 7 characters long.
-     * unitDescription cannot be blank and must be less than 25 characters long 
-     * creditPoints cannot be < 0.
-
-     * If any of the validations fail, print a message on the screen and assign the default value specified in the default constructor.
-     * The method then assigns these values to the unit fields in the enrolment class.
-     * Hint: You can achieve this by obtaining the object first from the Enrolment class via a method and then 
-     * using that object to call another method in the Unit class to set the value. Which method would that be? 
-     * */
+        // Integer Stream: Sequence of Numbers | From a Range of 0 to ArraListSize - For each element excute process) 
+        IntStream.range(0, enrollments.size()).forEach(index -> inputUnitDetailsOnce(index, enrollments.get(index)));
+    }
 
     /** 
-     * Custom Method: Get User to Input the Unit Details
+     * Custom Method: Get User to Input the Unit Details Once
      * @param enrollment The Enrollment Object to be Updated by th User; Passed as an Object of the Enrollment Class
+     * @throw IndexOutOfBoundsException The index is out of bounds of the Array List
      */
-    public void inputUnitDetails(Enrollment enrollment)
+    public void inputUnitDetailsOnce(int index, Enrollment enrollment)
     {   
         // Get the Unit Object from the Enrollment Class
-        Unit unit = enrollment.getUnit(); 
+        Unit[] units = enrollment.getUnits(); 
         Input inputFromUser = new Input(); 
         Validation validateInput = new Validation(); 
         
         String[] inputFields = {"Unit Code", "Unit Desecription", "Credit Score"}; 
         String userResponse; 
+
+        // May need to verify if the Index is within Bounds
+        if (index < 0 || index >= this.enrollments.size()) {
+            throw new IndexOutOfBoundsException("The index is out of bounds of the Array List"); 
+        }
         
         // Get User Input and Validate the Input 
         for (int iterI = 0; iterI < inputFields.length; iterI++)
@@ -133,34 +168,31 @@ public class University
                     case 0: // Unit Code cannot be blank and must be exactly 7 characters long.  
                     userResponse = inputFromUser.acceptStringInput("Input " + inputFields[iterI] + ":"); 
                     if (validateInput.isBlank(userResponse) || userResponse.length() != 7){     
-                        userResponse = ""; // Default Constructor also initializes null 
                         System.out.println("Validation Failed: Unit Code cannot be Blank and must be Exactly 7 Characeters Long");
                     } else {
+                        units[index].setUnitCode(userResponse);
                         validInput = true; 
                     }
-                    unit.setUnitCode(userResponse);
                     break; 
                     
                     case 1: // Unit Description Must not be Blank and must be less than 25 Characters in Length
                     userResponse = inputFromUser.acceptStringInput("Input " + inputFields[iterI] + ":"); 
-                    if (validateInput.isBlank(userResponse) || userResponse.length() > 25) {
-                        userResponse = ""; // Default Constructor also initializes null 
+                    if (validateInput.isBlank(userResponse) || userResponse.length() > 25) { 
                         System.out.println("Validation Failed: Unit Desecription must not be Blank and be less than 25 Characters in Length"); 
                     } else {
+                        units[index].setUnitDescription(userResponse);
                         validInput = true; 
                     }
-                    unit.setUnitDescription(userResponse);
                     break; 
                     
                     case 2: // Credit Point Must not be smaller than 0 
                     int userResponseInteger = inputFromUser.acceptIntegerInput("Input " + inputFields[iterI] + ":"); 
-                    if (userResponseInteger < 0) {
-                        userResponseInteger = 0; 
+                    if (userResponseInteger < 0) { 
                         System.out.println("Validation Failed: The Credit Score must not be lesser than the value 0 "); 
                     } else {
-                        validInput = false; 
+                        units[index].setCreditPoints(userResponseInteger);
+                        validInput = true; 
                     }
-                    unit.setCreditPoints(userResponseInteger);
                     break; 
 
                     default: 
@@ -169,6 +201,42 @@ public class University
                 }
             }
         }
+    }
+    
+    /** 
+     * Custom Method: Removes an Enrollment Object from Array List
+     * @param index The Index of the Enrollment Objec to be Removed from the Array List
+     * @throw IndexOutOfBoundsException The Index is out of bounds of the Array List 
+     */
+    public void removeEnrollment(int index)
+    {
+        if (index < 0 || index >= this.enrollments.size())
+        {
+            throw new IndexOutOfBoundsException("The Index is out of bounds of the Array List"); 
+        }
+        this.enrollments.remove(index); 
+    }
+
+    /** Mutator Method: Set Enrolment Details
+     * @param enrollments An ArrayList of Enrollment Objects of the Enrollment Class
+     */
+    public void setEnrollment(ArrayList<Enrollment> enrollments)
+    {
+        this.enrollments = enrollments; 
+    }
+
+    /**
+     * Mutator Method: Set Specific Enrollment Details
+     * @param index The Index of the Enrollment Object to be Set in the Array List
+     * @param enrollment The new Enrollment object to be Updated at the Index
+     * @throw The Index is out of bounds for the Enrollment ArrayList
+     */
+    public void setSpecificEnrollment(int index, Enrollment enrollment)
+    {
+        if (index < 0 || index >= this.enrollments.size()) {
+            throw new IndexOutOfBoundsException("The Index is out of bounds for the Enrollment ArrayList"); 
+        }
+        this.enrollments.set(index, enrollment);
     }
 
     /*
@@ -186,8 +254,8 @@ public class University
     public static void main(String[] args)
     {   
         Input userInput = new Input(); 
-        Enrollment enrollment = new Enrollment(); 
-        University university = new University(enrollment); 
+        ArrayList<Enrollment> enrollments = new ArrayList<Enrollment>(); 
+        University university = new University(enrollments); 
 
         System.out.println("\nThis is the University Enrollment Program."); 
         String userSelection = userInput.acceptStringInput("Please Enter:\n1 - To Enroll a Student\n2 - To Exit the Program"); 
@@ -200,11 +268,11 @@ public class University
                 newEnrollmentDate = ""; // Default Value of String in Default Constructor is also Null
                 System.out.println("The Enrollment Date should not be Blank"); 
             }
-            enrollment.setDate(newEnrollmentDate);
+            enrollments.setDate(newEnrollmentDate);
             
             // Get user to Input the Student and Unit Details
-            university.inputStudentDetails(enrollment);
-            university.inputUnitDetails(enrollment);
+            university.inputStudentDetails(enrollments);
+            university.inputUnitDetails(enrollments);
 
             // Display the University Object Information
             System.out.println(university.displayUniversity());
