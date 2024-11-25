1240ba6fc1834a122c87c40517c60567210f16df
Tristan Sim <tsym0098@gmail.com>
2024-11-25 22:59:42 +0800

    Map Updates

--- Java Programming/Assignment Java Jumper/src/Map.java
+++ Java Programming/Assignment Java Jumper/src/Map.java
@@ -8,22 +8,19 @@
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.logging.Level; 
 import java.util.logging.Logger;
 
 public class Map {
     
     // Fields - Constants
-    private static final Logger LOGGER = Logger.getLogger(Map.class.getName());  // For Internal Debugging Purposes
     private static final Random RANDOMIZER = new Random();   // Generate Random Numbers
-    private static final boolean DEBUG_FLAG = true;          // Internal Flag for Debugging Purposes (System.out.print)
     private static final int NUMBER_OF_BUILDINGS = 15;       // The Number of Building a Map can HAVE
     private static final int MAX_FUEL_CELLS = 4;             // Maximum Number of Fuel Cells a Game can have
     private static final int FUEL_CELL_LIFETIME = 3;         // The Lifetime (Turns) until the Fuel Cell expires for a Building 
     
     // Fields
-    private Building[] buildings = new Building[NUMBER_OF_BUILDINGS]; 
-    private int[] fuelCellBuildings = new int[NUMBER_OF_BUILDINGS]; 
+    private Building[] buildings;
+    private int[] fuelCellBuildings;
     private int frozenBuildingIndex; 
     private int portalBuildingIndex; 
     private int webTrapBuildingIndex; 
@@ -48,7 +45,6 @@ public class Map {
     
     /**
      * Non-Default Constructor for Map
-     * This Constructor will Automatically Handle Invalid Bounds or Null Objects inside the Building Array
      * @param buildings Array: Building Objects to be Constructed
      * @param fuelCellBuildings HashMap: Key represents the building index (Integer). Value represents the remaining turns for the fuel cell (Integer).
      * @param frozenBuildingIndex Integer: The Index of which Building will be Frozen
@@ -60,60 +56,67 @@ public class Map {
         this.frozenBuildingIndex = frozenBuildingIndex;
         this.portalBuildingIndex = portalBuildingIndex; 
         this.webTrapBuildingIndex = webTrapBuildingIndex; 
-        this.buildings = new Building[NUMBER_OF_BUILDINGS];   
-        this.fuelCellBuildings = new int[NUMBER_OF_BUILDINGS]; 
-        
-        if (buildings.length != NUMBER_OF_BUILDINGS)  // Software Debugging Purposes
-        {
-            debugException(new IndexOutOfBoundsException("Index Out of Bounds")); 
-        }
-        for (int i = 0; i < NUMBER_OF_BUILDINGS; i++) // Prevent Null Objects
-        {   
-            if (buildings.length > i && buildings[i] != null) {
-                this.buildings[i] = new Building(buildings[i].getHeight(), buildings[i].getPortal(), buildings[i].getFuelCell(), buildings[i].getWebTrap(), buildings[i].getFrozen());
-            } else {
-                this.buildings[i] = new Building(1, false, false, false, false); 
-            }
-            if (fuelCellBuildings.length > i) {
-                this.fuelCellBuildings[i] = fuelCellBuildings[i]; 
-            } 
-        }
+        this.buildings = buildings;
+        this.fuelCellBuildings = fuelCellBuildings; 
     }
 
     /** 
      * Custom Method: Create New Map
      * Will also randomize the Building Height, Effects, Portals & Traps 
+     * Method also prevents Duplication of Effects on the same building
      * @param playerLocation Player Location/Index where the player is currently at 
     */
     public void createMap(int playerLocation)
     {   
-        // This will track the Index of Which Building has Effects & Players
+        // This will track the Index to Prevent Duplication of Effects on the same building
         ArrayList<Integer> occupiedBuildings = new ArrayList<Integer>();  
         occupiedBuildings.add(playerLocation); 
 
         int[] randomIndex = new int[3]; 
         for (int i = 0; i < randomIndex.length; i++)
-        {
+        {   
             do {
                 randomIndex[i] = RANDOMIZER.nextInt(NUMBER_OF_BUILDINGS); 
             } while (occupiedBuildings.contains(randomIndex[i]));
+            occupiedBuildings.add(randomIndex[i]); 
         }
         
         randomizeBuildingsHeight();
+        setPortalBuilding(randomIndex[0]);
+        setFrozenBuilding(randomIndex[1]);
+        setWebTrapBuilding(randomIndex[1]);
     }
 
-    /**
-     * Private Custom Method: Print Runtime Errors using Logger 
-     * @param exception Exception: The Exception Thrown by Java to be display in the console 
-     */
-    private static void debugException(Exception exception)
+    /** 
+     * Custom Method: Create New Map with Existing Building Data, Example Below:
+     * Height, Portal, Fuel Cells, Trap, Frozen
+     * "2,False,False,False,False"
+     * @param playerLocation Player Location/Index where the player is currently at 
+     * @param buildingData String[]; An Array of String Lines containing Building Information
+
+    */
+    public void createMap(int playerLocation, String[] buildingData) 
+        throws NumberFormatException
     {   
-        if (DEBUG_FLAG) {
-            LOGGER.log(Level.SEVERE, exception.getMessage(), exception);
-            throw new RuntimeException(exception); 
+        for (int i = 0; i < buildingData.length; i++)
+        {
+            String[] stringSegments = buildingData[i].split(",");
+            this.buildings[i].setHeight(Integer.parseInt(stringSegments[0])); 
+
+            if (Boolean.parseBoolean(stringSegments[1].toLowerCase())) {
+                setPortalBuilding(i);
+            } else if (Boolean.parseBoolean(stringSegments[3].toLowerCase())) {
+                setWebTrapBuilding(i);
+            } else if (Boolean.parseBoolean(stringSegments[4].toLowerCase())) {
+                setFrozenBuilding(i);
+            }
+
+            if  (Boolean.parseBoolean(stringSegments[2].toLowerCase())) {
+                setSpecificFuelCellBuilding(i, FUEL_CELL_LIFETIME); 
+            }
         }
     }
-    
+
     /**
      * Display Method: Display the Fields of the Map Object
      */
@@ -189,33 +192,6 @@ public class Map {
     }
 
     /** 
-     * Custom Method: Randomize the Frozen Building Index
-     * @return Integer: The new Randomize Index of the Frozen Building
-    */
-    public void randomizeFrozenBuilding()
-    {
-        this.frozenBuildingIndex = RANDOMIZER.nextInt(NUMBER_OF_BUILDINGS-1);
-    }
-
-    /** 
-     * Custom Method: Randomize the Portal Building Index
-     * @return Integer: The new Randomize Index of the Portal Building
-    */
-    public void randomizePortalBuilding()
-    {
-        this.portalBuildingIndex = RANDOMIZER.nextInt(NUMBER_OF_BUILDINGS-1);
-    }
-
-    /** 
-     * Custom Method: Randomize the Web Trap Building Index
-     * @return Array<Building>: Returns an Array Containing the Building Objects 
-    */
-    public void randomizeWebTrapBuilding()
-    {
-        this.webTrapBuildingIndex = RANDOMIZER.nextInt(NUMBER_OF_BUILDINGS - 1); 
-    }
-
-    /** 
      * Custom Method: Reset the Fuel Cells Lifetime & Building Index
      * Does not spawn on another building with effect like Player, Frozen, Traps & Portals are located
      * @param occupiedBuildings ArrayList<Integer>: An Array List of Integers where Player, Frozen, Traps & Portals are located
@@ -252,6 +228,73 @@ public class Map {
     public void removeFuelCell(int index)
     {
         this.fuelCellBuildings[index] = 0; 
+    }
+
+    /**
+     * Mutator Method: Set Buildings
+     * @param buildings Building[]: Accepts an Array of Buildings 
+     */
+    public void setBuildings(Building[] buildings)
+    {
+        this.buildings = buildings; 
+    }
+
+    /**
+     * Mutator Method: Set Frozen Building Index
+     * @param index Integer: The index of the building to be frozen
+     */
+    public void setFrozenBuilding(int index)
+    {
+        this.frozenBuildingIndex = index; 
+        this.buildings[index].setFrozen(true);
+    }
+
+    /**
+     * Mutator Method: Set Fuel Cell Building
+     * @param index Integer[]: An Array of integers containing the Fuel Cell Lifetime for each building
+     */
+    public void setFuelCellBuildings(int[] fuelCellBuildings)
+    {
+        this.fuelCellBuildings = fuelCellBuildings;
+        for (int i = 0; i < this.fuelCellBuildings.length; i ++)
+        {
+            if (this.fuelCellBuildings[i] > 0) {
+                this.buildings[i].setFuelCell(true);
+            } else {
+                this.buildings[i].setFuelCell(false);
+            }
+        }
+    }
+
+    /**
+     * Mutator Method: Set Specific Fuel Cell Building
+     * @param index Integer: The Index of the Building to Set
+     * @param fuelCellLifetime Integer: The Lifetime of the Fuel Cell to Set
+     */
+    public void setSpecificFuelCellBuilding(int index, int fuelCellLifetime)
+    {
+        this.fuelCellBuildings[index] = Math.max(0, Math.min(fuelCellLifetime, FUEL_CELL_LIFETIME));
+        this.buildings[index].setFuelCell(true);
+    }
+
+    /**
+     * Mutator Method: Set Portal Building Index
+     * @param index Integer: The index of the building with the Portal
+     */
+    public void setPortalBuilding(int index)
+    {
+        this.portalBuildingIndex = index;
+        this.buildings[index].setPortal(true);
+    }
+
+    /**
+     * Mutator Method: Set Portal Building Index
+     * @param index Integer: The index of the building with the Portal
+     */
+    public void setWebTrapBuilding(int index)
+    {
+        this.webTrapBuildingIndex = index;
+        this.buildings[index].setWebTrap(true);
     }
 
     /**
