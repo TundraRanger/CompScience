08b230ca70ece0d87e68fd44710d008ed02b1dc2
Tristan Sim <tsym0098@gmail.com>
2024-11-29 01:03:41 +0800

    Updates

--- Java Programming/Assignment Java Jumper/src/Player.java
+++ Java Programming/Assignment Java Jumper/src/Player.java
@@ -1,3 +1,38 @@
-public class Player {
+
+public class Player 
+{
+    // Fields
+    private int location;
+    private Device device; 
+
+    /**
+    * Default Constructor
+    */
+
+    /**
+    * Non-Default Constructor
+    */
+ 
+    /**
+    * Display Method: Display String of the Player Details
+    * @return String: Containing the Player Details
+    */
     
+    /**
+     * Accesor Method: Get Location of the Player (Building Index)
+     * @return int: Player Location as a Building Index
+     */
+    
+    /**
+     * Accesor Method: Get the Device Object
+     * @return Device Object
+     */
+    
+    /**
+     * Custom Method: Move the Player Left
+     * @param currentBuilding int: The Current Building Index the Player is currently on
+     * @param targetBuilding int: The Target Building Index for the next Hop
+     */
+    
+
 }
