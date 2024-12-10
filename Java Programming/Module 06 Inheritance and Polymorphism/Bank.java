c24f366c7fb055297e74ab7d5e19737aba30702f
Tristan Sim <tsym0098@gmail.com>
2024-12-10 22:29:04 +0800

    Subtyping

--- Java Programming/Module 06 Inheritance and Polymorphism/Bank.java
+++ Java Programming/Module 06 Inheritance and Polymorphism/Bank.java
@@ -20,5 +20,15 @@ public class Bank
 
         Account account1 = new Account(); 
         System.out.println("Directly Read Protected Message of Account Class: " + account1.displayProtectedMessage());
+
+        System.out.println("Section Below will cover Subtyping of a Super Class... ");
+        System.out.println("<Class Name> <Variable Name> (Static Type) = new <Class Constructor>; (Dynamic Type)");
+        
+        Account savingAccount2 = new Savings(100, 8000.0, 1.5, 100000, 0); 
+
+        System.out.println("Account Number(Inherited from Super Class 'Accout'): " + savingAccount2.getAccountNumber());
+        System.out.println("Account Balance(Inherited from Super Class 'Accout'): " + savingAccount2.getBalance());
+        // System.out.println("Account Interest(From Class itself): " + savingAccount2.getInterest()); This won't work: Object Type is still Account
+
     }
 }
