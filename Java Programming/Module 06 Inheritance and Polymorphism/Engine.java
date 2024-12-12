3aed3c38160d7cee61a67afaaeae7a21bb6f5469
Tristan Sim <tsym0098@gmail.com>
2024-12-12 23:10:30 +0800

    Updates to Engine

--- Java Programming/Module 06 Inheritance and Polymorphism/Engine.java
+++ Java Programming/Module 06 Inheritance and Polymorphism/Engine.java
@@ -1,37 +1,140 @@
-
 /**
- * Inheritance & Polymorphism Example
- * Engine Class is a Server Class to Vehicle
+ * Project: Inheritance & Polymorphism (Vehicle - Mini Practice)
+ * Represents an engine with various attributes such as model, type, and specifications.
+ * Fields are Encapsulated in privates attributes, accesibles by the accesors and Mutator Methods.
  * @author Tristan Sim
  * @version 1.00
  */
 
-public class Engine 
-{
-    // Fields 
-    private String model;
-    private String countryOfOrigin; 
-    private String type; 
-    private double displacement;
-    private double hoursepower; 
+public class Engine {
 
-    public Engine()
-    {
+    // Fields
+    private String model;
+    private String countryOfOrigin;
+    private String type;
+    private double displacement;
+    private double hoursepower;
+
+    /**
+     * Default constructor that initializes the engine with default values.
+     */
+    public Engine() {
         this.model = "FooBar";
         this.countryOfOrigin = "United States";
         this.type = "Petrol";
-        this.displacement = 3.0; 
-        this.hoursepower = 150; 
+        this.displacement = 3.0;
+        this.hoursepower = 150;
     }
 
-    public Engine(String model, String countryOfOrigin, String type, double displacement, double hoursepower)
-    {
+    /**
+     * Parameterized constructor to initialize the engine with specific values
+     * @param model           The model of the engine
+     * @param countryOfOrigin The country where the engine was manufactured
+     * @param type            The type of engine (e.g., Petrol, Diesel)
+     * @param displacement    The engine's displacement in liters
+     * @param hoursepower     The engine's horsepower
+     */
+    public Engine(String model, String countryOfOrigin, String type, double displacement, double hoursepower) {
         this.model = model;
-        this.countryOfOrigin = countryOfOrigin; 
-        this.type = type; 
-        this.displacement = displacement; 
-        this.hoursepower = hoursepower; 
+        this.countryOfOrigin = countryOfOrigin;
+        this.type = type;
+        this.displacement = displacement;
+        this.hoursepower = hoursepower;
     }
 
+    /**
+     * Display Method: Displays the State of the Engine Class
+     * @return String: The State of the Engine Class, delimited by ':' & '\n'
+     */
+    public String display() 
+    {
+        StringBuffer stringBuffer = new StringBuffer(); 
+        stringBuffer.append("Model: ").append(this.model).append("\n"); 
+        stringBuffer.append("Country Of Origin: ").append(this.countryOfOrigin).append("\n"); 
+        stringBuffer.append("Type: ").append(this.type).append("\n"); 
+        stringBuffer.append("Displacement: ").append(this.displacement).append("\n"); 
+        stringBuffer.append("Horsepower: ").append(this.hoursepower).append("\n"); 
     
+        return stringBuffer.toString(); 
+    }
+
+    /**
+     * Gets the model of the engine.
+     * @return The model of the engine.
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * Sets the model of the engine.
+     * @param model The model to set.
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * Gets the country where the engine was manufactured.
+     * @return The country of origin.
+     */
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    /**
+     * Sets the country where the engine was manufactured.
+     * @param countryOfOrigin The country of origin to set.
+     */
+    public void setCountryOfOrigin(String countryOfOrigin) {
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    /**
+     * Gets the type of the engine (e.g., Petrol, Diesel).
+     * @return The type of the engine.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type of the engine.
+     * @param type The type to set.
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets the displacement of the engine in liters.
+     * @return The engine's displacement.
+     */
+    public double getDisplacement() {
+        return displacement;
+    }
+
+    /**
+     * Sets the displacement of the engine in liters.
+     * @param displacement The displacement to set.
+     */
+    public void setDisplacement(double displacement) {
+        this.displacement = displacement;
+    }
+
+    /**
+     * Gets the horsepower of the engine.
+     * @return The engine's horsepower.
+     */
+    public double getHorsepower() {
+        return hoursepower;
+    }
+
+    /**
+     * Sets the horsepower of the engine.
+     * @param hoursepower The horsepower to set.
+     */
+    public void setHorsepower(double hoursepower) {
+        this.hoursepower = hoursepower;
+    }
 }
--- Java Programming/Module 06 Inheritance and Polymorphism/Vehicle.java
+++ Java Programming/Module 06 Inheritance and Polymorphism/Vehicle.java
@@ -1,4 +1,3 @@
-
 /**
  * Inheritance & Polymorphism Example
  * Vehicle is a Parent Class of Cars and Trucks (Multi-Level Inheritance)
@@ -9,23 +8,56 @@
 public abstract class Vehicle
 {
     // Fields
-    protected short numberOfSeats; 
-    protected short yearOfManufacture; 
     protected String brand; 
     protected String model; 
     protected Engine engine; 
-
+    protected short numberOfSeats; 
+    protected short yearOfManufacture; 
+    
+    /**
+     * Non-Parameterized/Default constructor to initialize the Vehicle Class
+     */
     public Vehicle()
-    {
-        this.numberOfSeats = 2; 
-        this.yearOfManufacture = 2000; 
+    {   
         this.brand = "Mercedes"; 
         this.model = "C300"; 
         this.engine = new Engine(); 
+        this.numberOfSeats = 2; 
+        this.yearOfManufacture = 2000; 
     }
-
-    public Vehicle(short numberOfSeats, short yearOfManufacture, String brand, String model, String engineType, double enginePower)
+    
+    /**
+     * Parameterized/Non-Default constructor to initialize the Vehicle Class with specific values
+     * @param numberOfSeats      The Number of Seats the Vehicle has
+     * @param yearOfManufacture  The Year of the Manufacture
+     * @param brand              The Brand of the Vehicle
+     * @param model              The Model of the Vehicle
+     * @param engine             The Engine Object of the Vehicle Class
+     */
+    public Vehicle(short numberOfSeats, short yearOfManufacture, String brand, String model, Engine engine)
+    {   
+        this.brand = brand; 
+        this.model = model; 
+        this.engine = engine; 
+        this.numberOfSeats = numberOfSeats; 
+        this.yearOfManufacture = yearOfManufacture; 
+    }
+    
+    /**
+     * Display Method: Displays the State of the Engine Class
+     * @return String: The State of the Engine Class, delimited by ':' & '\n'
+     */
+    public String display() 
     {
-
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("Brand: ").append(this.brand).append("\n");
+        stringBuffer.append("Model: ").append(this.model).append("\n");
+        stringBuffer.append("Year of Manufacture: ").append(this.yearOfManufacture).append("\n");
+        stringBuffer.append("Number of Seats: ").append(this.numberOfSeats).append("\n");
+        stringBuffer.append("Engine Details:\n").append(this.engine.display()); 
+        
+        return stringBuffer.toString();
     }
+    
+
 }
