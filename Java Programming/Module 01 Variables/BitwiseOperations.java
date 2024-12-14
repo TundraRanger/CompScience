d957b9b61d91f12b01e0b9b7a350d099a748ce58
Tristan Sim <tsym0098@gmail.com>
2024-12-14 16:26:20 +0800

    Bit Flip Operation

--- Java Programming/Module 01 Variables/BitwiseOperations.java
+++ Java Programming/Module 01 Variables/BitwiseOperations.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
 
 public class BitwiseOperations
-{
-    public static void main(String[] args)
+{   
+    public BitwiseOperations()
     {
+
+    }
+    public static void main(String[] args)
+    {   
+        BitwiseOperations bitOperation = new BitwiseOperations(); 
         byte[] bits = {0, 1, 0, 0, 0, 0, 0, 1}; // Binary Value of '65'
         byte byteValue = 0; // 8-bit Unsigned Value
         
@@ -17,26 +22,36 @@ public class BitwiseOperations
 
             if (i == 0){
                 System.out.println("\nIteration 0 (Before Bit Shift Operation)"); 
-                System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + parseBits(byteValue, 8) + ")");
+                System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + bitOperation.parseBits(byteValue, 8) + ")");
             }
             else if (i == 2) {
                 System.out.println("\nIteration 2 (Bit Shift Right to Left Twice)"); 
-                System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + parseBits(byteValue, 8) + ")");
+                System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + bitOperation.parseBits(byteValue, 8) + ")");
             }
         } 
         
         System.out.println("\nAfter Complete 8-bit Shift Operation:"); 
-        System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + parseBits(byteValue, 8) + ")");
+        System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + bitOperation.parseBits(byteValue, 8) + ")");
+
+        System.out.println("\n8-Bit Manipulation by Flipping Bits:");
+        System.out.println("Flip Byte = " + byteValue + " to a Value of '88'");
         
+        byteValue = bitOperation.flipBit(byteValue, 8, 3); // Flip Bit 3
+        byteValue = bitOperation.flipBit(byteValue, 8, 4); // Flip Bit 4 
+        byteValue = bitOperation.flipBit(byteValue, 8, 7); // Flip Bit 7 
+        
+        System.out.println("Value of Byte = " + byteValue + " (8-Bit Form: " + bitOperation.parseBits(byteValue, 8) + ")");     
+
     }
 
-    public static ArrayList<String> parseBits(Byte byteValue, int bitLength)
+    public ArrayList<String> parseBits(Byte byteValue, int bitLength)
     {  
        ArrayList<String> bitData = new ArrayList<String>(); 
 
        // Convert into Byte into a Binary String
        // & 0xFF makes sure you're only working with the actual 8 bits of the byte, ignoring negative signs or extra bits.
-       String binaryString = Integer.toBinaryString(byteValue & 0xFF); 
+       // 0xFF is a hexadecimal literal representing the binary value 11111111 | Masking = Used to isolate the lower 8 bits of a number in operations 
+       String binaryString = Integer.toBinaryString(byteValue & 0xFF);  
        
        // Ensures the Data is 8-Bits; else Pad Data with "0"
        while (binaryString.length() < bitLength)
@@ -51,5 +66,29 @@ public class BitwiseOperations
        }
 
        return bitData; 
+    }
+
+    public Byte flipBit(Byte byteValue, int bitLength, int targetBit)
+    {   
+        // Convert Byte to Binary String
+        String binaryString = Integer.toBinaryString(byteValue & 0xFF); 
+        while (binaryString.length() < bitLength) // Padding to form a Complete 8-Bit
+        {
+            binaryString = "0" + binaryString;
+        }
+
+        StringBuffer bitData = new StringBuffer(binaryString); 
+
+        if (targetBit >= 0 && targetBit < bitLength)
+        {
+            char flippedBit = binaryString.charAt(targetBit) == '1' ? '0' : '1'; 
+            bitData.setCharAt(targetBit, flippedBit);
+        }
+        
+        // Radix: The (Base) for the Number System being used to Intepret the String
+        // Default: 10 (Decimal) | 16 (Hexidecimal)  |  2 (Binary)
+        int results = Integer.parseInt(bitData.toString(), 2);
+
+        return (byte) results; 
     }
 }
